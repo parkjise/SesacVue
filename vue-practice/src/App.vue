7a15db3d8c8129999475c1180faed1e1aacf21f8
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-10-31 18:07:15 +0900

    1031

---
+++ vue-practice/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Hello World{{ userName }}</h1>
+    <h1 v-text="userName"></h1>
+    <p>{{ year }}</p>
+    <p>{{ user.name }}{{ user.job }}{{ user.age }}</p>
+    <p v-html="button"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      userName: "parkjise",
+      year: 2024,
+      user: {
+        name: "jise Park",
+        job: "Designer",
+        age: 44,
+      },
+      button: "<button>버튼</button>",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
