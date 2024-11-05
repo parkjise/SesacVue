4c14a6e4b321dd1d7855d9bfd3cb4b61e883bd76
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-05 16:46:31 +0900

    1105

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,95 +1,20 @@
-<template>
-  <div>
-    <h1 id="title">Hello World{{ userName }}</h1>
-    <h1 v-text="userName" v-bind:id="dynamicId"></h1>
-    <p>{{ year }}</p>
-    <p>{{ user.name }}{{ user.job }}{{ user.age }}</p>
-    <p v-html="button"></p>
-    <a v-bind:href="url">naver</a>
-    <img v-bind:src="image.src" v-bind:alt="image.alt">
-    <input v-bind:type="inputType">
-    <p v-bind:style="pStyle">V-bind Test</p>
-    <p v-bind:style="{color:'red',fontSize: '50px'}">V-bind Test</p>
-    <p v-bind:style="{color:'red',fontSize:`${basicSize}px`}">V-bind Test</p>
-    <h1 v-bind:style="titleStyle">Hello World{{ userName }}</h1>
-    <h1 v-bind:style="[titleStyle,basicStyle]">Hello World{{ userName }}</h1>
-    <h2 :class="textDecoration" class="text-orange">Line-through</h2>
-    <h2 :class="isDone === true ? 'line-through' : 'highlight'">Line-through</h2>
-    <h2 :class="{highlight : isDone === true}">Object형태의 동적 클래스</h2>
-    <h2 :class="{
-      highlight : isDone === true,
-      'text-orange' : userName === 'parkjise'}">Object형태의 동적 클래스</h2>
-      <h2 :class="[isDone===true ? 'line-through' : 'highlight',userName === 'parkjise' ? 'text-green' : 'highlight']">Array 형태의 동적 클래스 부여</h2>
-  </div>
-</template>
-
 <script>
+
+import ConRendering from './components/ConditionalRendering.vue'
+
 export default {
-  name: "App",
-  data() {
-    return {
-      userName: "parkjise",
-      year: 2024,
-      user: {
-        name: "jise Park",
-        job: "Designer",
-        age: 44,
-      },
-      button: "<button>버튼</button>",
-      dynamicId:"content",
-      url:"https:naver.com",
-      image:{
-        src:'https://palceimg.com/100/100/any',
-        alt:'random image'
-      },
-      inputType:"text",
-      pStyle:"font-size:46px;color:red",
-      basicSize:100,
-      basicStyle:{
-        background:'red'
-      },
-      titleStyle:{
-        'font-weight':'bold',
-        fontSize:'120px',
-        color:'blue',
-        border:"1px solid red"
-      },
-      textDecoration:'line-through',
-      isDone:false
-    };
-  },
-  // methods: {
-  //   isDoneT(){
-  //     if(this.isDone === true){
-  //       return highlight
-  //     }
-  //   }
-  // },
-};
+    components:{
+      ConRendering
+    },
+  }
 </script>
 
+<template>
+  <con-rendering></con-rendering>
+</template>
+
+
+
 <style>
-  #title{
-    background:yellow;
-    color:red;
-  }
-  #content{
-    background:pink;
-    color:white;
-  }
-
-  .line-through{
-    text-decoration: line-through;
-  }
-
-  .highlight{
-    color: #fff;
-    background-color: #000;
-  }
-  .text-orange{
-    color: orange;
-  }
-  .text-green{
-    color: green;
-  }
+  
 </style>
---
+++ vue-practice/src/components/ConditionalRendering.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2 v-if="showName">My name is {{ user.name }}</h2>
+    <h2 v-else>이름을 보여줄 수 없습니다</h2>
+    <h2 v-if="user.age > 20">당신은 성인입니다</h2>
+    <h2 v-else-if="user.age >14 && user.age < 20">당신은 청소년입니다</h2>
+    <h2 v-else>당신은 어린이 입니다.</h2>
+    <h2 v-if="!showName"> {{ user.name }}</h2>
+    <h2 v-show="!showName">{{ user.name }}</h2>
+    <ul>
+      <template v-if="question === 'frontend'">
+        <li>HTML은 재미있나요?</li>
+        <li>CSS은 재미있나요?</li>
+        <li>JavaScript은 재미있나요?</li>
+      </template>
+      <div v-else>
+        <li>JAVA은 재미있나요?</li>
+        <li>Python은 재미있나요?</li>
+        <li>C#은 재미있나요?</li>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: "App",
+  data() {
+    return {
+      question:'frontend',
+      showName:true,
+      user: {
+        name:'Jise Park',
+        age:44,
+        job:'Web Designer'
+      }
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style>
+
+</style>
