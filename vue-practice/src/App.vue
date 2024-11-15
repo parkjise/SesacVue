5556ac4c8744d3be63c9f956faad956054a147ce
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-15 17:25:14 +0900

    1115

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,15 +1,41 @@
-<script>
-import ComputedVue from "./components/ComputedVue.vue";
+<template>
+	<div>
+		<GreetingUser :username="username" :numberOfVisit="numberOfVisit" :siteInfo="siteInfo"/>
+		<!-- <GreetingUser username = "dooli" />
+		<GreetingUser username = "pororo"/>
+		<GreetingUser username = "monkey"/>
+		<GreetingUser/> -->
+		<button @click="changeName()">Change</button>
+	</div>
+</template>
 
+
+<script>
+import GreetingUser from "./components/GreetingUser";
 export default {
+	name: "App",
 	components: {
-		ComputedVue,
+		GreetingUser,
+	},
+	data(){
+		return {
+			username:"Hong gil dong",
+			numberOfVisit:80,
+			siteInfo:{
+				name:"Vue Practice",
+				teacher:undefined,
+				subject:"FrondEnd"
+			},
+			toggle:false,
+		}
+	},
+	methods: {
+		changeName(){
+			this.toggle = !this.toggle 
+			this.username = this.toggle ? "username" : 'jjTtest!!!!'
+		},
 	},
 };
 </script>
-
-<template>
-	<computed-vue></computed-vue>
-</template>
 
 <style></style>
---
+++ vue-practice/src/components/WatchVue.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Watchers</h1>
+    <h2>current money :: {{ money }}</h2>
+    <div>
+      <button @click="money += 100">earn money</button>
+      <button @click="money -= 100">spend moeny</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: "App",
+  data() {
+    return {
+      money:15000
+    };
+  },
+  computed:{
+    
+  },
+
+  watch:{
+    money(newValue,oldValue){
+      console.log(newValue,oldValue)
+      if(newValue > 16000){
+        console.log('dffdkfdjfkdjfkf')
+      }
+    }
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style>
+
+</style>
